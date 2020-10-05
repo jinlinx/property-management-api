3b476d91650ed93a26cf3d449346d83de5429f1b
Gang Zhang <gzhangx@hotmail.com>
2020-10-05 00:51:29 -0400

    fix id

--- api/models/workerComp.js
+++ api/models/workerComp.js
@@ -1,8 +1,8 @@
 module.exports={
     fields:
         [
-            {field: 'id', desc: 'Id', type: 'uuid'},
-            {field: 'workerID', desc: 'Id', type: 'uuid', required: true, isId: true, foreignKey: {table: 'workerInfo', field: 'workerID'}},
+            {field: 'id', desc: 'Id', type: 'uuid', isId: true, },
+            {field: 'workerID', desc: 'Id', type: 'uuid', required: true, foreignKey: {table: 'workerInfo', field: 'workerID'}},
             {field: 'schedule', desc: 'Schedule, Monthly usually'},
             {field: 'type', desc: 'Percent or amount'},
             {field: 'amount', desc: 'Percent', type: 'decimal'},
