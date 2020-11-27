d3c89b124150959f065b2c5e604a083bc637556c
Gang Zhang <gzhangx@hotmail.com>
2020-11-26 20:45:31 -0500

    adding import payments

---
+++ api/models/importPayments.js
@@ -0,0 +1,9 @@
+module.exports = {
+    fields:
+        [
+            { field: 'date', desc: 'Date', type: 'date' },
+            { field: 'amount', desc: 'Amount', type: 'decimal' },
+            { field: 'name', desc: 'Name', },
+            { field: 'notes', desc: 'Notes', },
+        ]        
+};
