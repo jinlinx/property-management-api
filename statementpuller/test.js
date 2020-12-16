b6ca77818265edb4f97fd4cc7341d08b6c745bd8
Gang Zhang <gzhangx@hotmail.com>
2020-12-16 15:41:41 -0500

    end properly

--- statementpuller/test.js
+++ statementpuller/test.js
@@ -1,8 +1,10 @@
 const paypal = require('./paypal');
+const venmo = require('./venmo');
+const db = require('../api/lib/db');
 
+//return venmo.doVenmo().catch(err => console.log(err))
 
-paypal.doPaypal().catch(err => {
-    console.log(err);
-}).then(() => {
-    console.log('done');
-})
+return paypal.doPaypal().catch(err => console.log(err)).then(() => console.log('done'))
+    .then(() => {
+        db.conn.end();
+});
