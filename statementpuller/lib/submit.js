7916a0830f3b041e126482c23b56b4aa1ebb45c8
Gang Zhang <gzhangx@hotmail.com>
2020-12-18 11:20:47 -0500

    new formats

--- statementpuller/lib/submit.js
+++ statementpuller/lib/submit.js
@@ -4,21 +4,24 @@ const Promise = require('bluebird');
 const uuid = require('uuid');
 const sheet = require('./getSheet').createSheet();
 const moment = require('moment');
-const sheetId = '1xFCW8QsdfWRMjzXcUcXwe4HIfWNShgkrHE7UbKGgwLc';
+const sheetId = '1sKppFHJy_MRRgHuV2PzhliSzuje7O0Rb-ntiOrLDVPA';
 async function submit(datas) {
     let cur = 0;
-    await sheet.appendSheet(sheetId, `'Sheet1'!A1`, datas.map(data => [data.date, data.amount, data.name, data.notes, data.source]));
+    //await sheet.appendSheet(sheetId, `'Sheet1'!A1`, datas.map(data => [data.date, data.amount, data.name, data.notes, data.source]));
     const nameSourceLeaseRows = await db.doQuery(`select ptm.tenantID, ptm.name, ptm.source , lti.leaseID,
-    t.firstName, t.lastName    
+    t.firstName, t.lastName, h.address, h.city, h.state, h.zip
     from payerTenantMapping ptm
-    inner join tenantInfo t on ptm.tenantID = t.tenantID
+    inner join tenantInfo t on ptm.tenantID = t.tenantID    
     inner join leaseTenantInfo lti on ptm.tenantID = lti.tenantID
+    inner join leaseInfo l on l.leaseID = lti.leaseID
+    inner join houseInfo h on h.houseID = l.houseID
     where
     ${datas.map(d => '(ptm.name=? and ptm.source=?)').join(' or ')}`, datas.reduce((acc, d) => { 
         acc.push(d.name);
         acc.push(d.source);
         return acc;
     }, []));
+    console.log(nameSourceLeaseRows)
     const getMapKey = r => `${r.name}-${r.source}`;
     const nameSoruceLeaseMapping = nameSourceLeaseRows.reduce((acc, r) => {
         acc[getMapKey(r)] = r;
@@ -40,6 +43,8 @@ async function submit(datas) {
         //     console.log(`done ${me}/${datas.length}`);
         //     return res.body;
         // });
+        data.date = moment(data.date, 'YYYY-MM-DD').toDate();
+        data.amount = parseFloat(data.amount);
         const { date, amount, name, notes, source } = data;
         const parms = [date, amount, name, notes || '', source || ''];
         const existing = await db.doQuery(`select 1 from importPayments where date=? and amount=? and name=? and notes=? and source=?`,
@@ -50,18 +55,17 @@ async function submit(datas) {
                 imported: 0,
             });
         } else {
-
             const id = uuid.v1();
             const key = getMapKey(data);
             const matched = nameSoruceLeaseMapping[key];
             let imported = 'imported';
             if (matched) {
-                imported = 'imported and matched';
-                console.log(`matched ${name} on ${matched.firstName} ${matched.lastName} for ${amount} ${matched.leaseID}`);                
+                imported = 'matched';
+                console.log(`matched ${name} on ${matched.firstName} ${matched.lastName} for ${amount} ${matched.address}`);                
                 await db.doQuery(`insert into rentPaymentInfo(paymentID, receivedDate,receivedAmount,
                         paidBy,leaseID,created,modified,notes)
                         values(?,?,?,
-                        ?,?,now(),now(),?)`, [id, moment(date).toDate(), amount,
+                        ?,?,now(),now(),?)`, [id, date, amount,
                     name, matched.leaseID, notes]);                                
                 await db.doQuery(`insert into importPayments (id,date, amount, name,notes, source, matchedTo) values(?,?,?,?,?,?,?)`,
                     [id].concat(parms).concat(id))
@@ -70,6 +74,7 @@ async function submit(datas) {
                     [id].concat(parms));
             }
             return ({
+                ...matched,
                 ...data,
                 id,
                 imported,
@@ -78,6 +83,14 @@ async function submit(datas) {
     }, { concurrency: 5 });
     const imported = allRes.filter(r => r.imported);
     console.log(`imported=${imported.length}, all itemps ${allRes.length} `);
+    const values = allRes.filter(r => r.imported === 'matched').map(data => {
+        const date = moment(data.date,'YYYY-MM-DD').format('YYYY-MM-DD');
+        return [data.address, date, date, data.amount, data.notes, data.name, data.source, data.id]
+    });
+
+    if (values.length) {
+        await sheet.appendSheet(sheetId, `'Total rent payment info'!A1`, values, 'USER_ENTERED');
+    }
     return allRes;
 }
 
