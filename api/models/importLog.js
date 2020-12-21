5b0299d29a62f9539c162c12126eb5517edda433
Gang Zhang <gzhangx@hotmail.com>
2020-12-20 21:05:25 -0500

    added import log

---
+++ api/models/importLog.js
@@ -0,0 +1,10 @@
+module.exports = {
+    fields:
+        [
+            { field: 'id', desc: 'Id' },
+            { field: 'source', desc: 'Source'},
+            { field: 'start', desc: 'End', type: 'datetime' },
+            { field: 'end', desc: 'End', type:'datetime'},
+            { field: 'msg', desc: 'Message', },
+        ]
+};
