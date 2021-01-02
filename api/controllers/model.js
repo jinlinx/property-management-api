ce34d4b076f69e135bf9bf894911eb7afac46046
Gang Zhang <gzhangx@hotmail.com>
2021-01-02 10:57:54 -0500

    test

--- api/controllers/model.js
+++ api/controllers/model.js
@@ -1,11 +1,13 @@
 const models = require('../models/index');
 
 async function getModel(req, res) {
-    try {
+  try {
+    console.log(`getModel -> ${req.query.name}`);
         const m = models[req.query.name];
-  
+      console.log(`getModel done ${req.query.name}`);
       return res.json(m);
     } catch (err) {
+      console.log('getModel Error');
       console.log(err);
       res.send(500, {
         name: req.query.name,
