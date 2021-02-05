79a8acccc91c35f986188284227ddfe26ef4dbce
Jinlin Xie <jinlinx@hotmail.com>
2021-02-04 23:08:48 -0500

    maintenance record

---
+++ api/models/maintenanceRecord.js
@@ -0,0 +1,13 @@
+module.exports = {
+    fields:
+        [
+            { field: 'id', desc: 'Id' , type: 'uuid', required: true, isId: true},
+            { field: 'date', desc: 'date', type: 'date' },
+            { field: 'description:', desc: 'description:' },
+            { field: 'amount', type: 'decimal', },
+            { field: 'houseID', desc: 'House ID', foreignKey: { table: 'houseInfo', field: 'houseID' } },
+            { field: 'category', desc: 'category' },
+            { field: 'workerID', desc: 'Id', type: 'uuid', required: true, foreignKey: {table: 'workerInfo', field: 'workerID'}},
+            { field: 'comment', desc: 'comment' },
+        ]
+};
