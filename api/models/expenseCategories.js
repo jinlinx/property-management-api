41547098a6172ba4e31926b8fb21a0159eb01fb0
Jinlin Xie <jinlinx@hotmail.com>
2021-02-04 23:35:42 -0500

    test

---
+++ api/models/expenseCategories.js
@@ -0,0 +1,7 @@
+module.exports = {
+    fields:
+        [
+            { field: 'expenseCategoryID', desc: 'Id' , type: 'int', required: true, isId: true},
+            { field: 'expenseCategoryName', desc: 'date' },
+        ]
+};
