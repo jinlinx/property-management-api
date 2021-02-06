3ee908b4ffdc59e8be2b7153387bd670ea5ef9ba
Jinlin Xie <jinlinx@hotmail.com>
2021-02-06 12:50:10 -0500

    test

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 const p = require('./api/gimports/importMatchPayments');
 const db = require('./api/lib/db');
+process.env.DBHOST = '192.168.1.41';
 p.importAndMatchPayments().then(() => {
     db.conn.end();
 })
