aabb1468a3df899c4b22c6ddae223577bb134fc0
Jinlin Xie <jinlinx@hotmail.com>
2021-02-06 13:26:05 -0500

    mimp payments

--- api/gimports/importMatchPayments.js
+++ api/gimports/importMatchPayments.js
@@ -8,129 +8,73 @@ const moment = require('moment');
 
 async function importAndMatchPayments() {
     async function readSheet() {
-        const res = await sheet.readRanges(sheetId, [`'MaintainessRecord'!A:L`]);
+        const res = await sheet.readRanges(sheetId, [`'Copy of Sheet11'!A:L`]);
         const data = get(res, 'data.valueRanges.0.values');
         return data;
     }
 
 
     return readSheet().then(async rentData => {
-        const result = rentData.reduce((acc, r) => {
-            if (!acc.posToName) {
-                acc.posToName = r.reduce((rr, v, i) => {
-                    rr[i] = v.trim().toLowerCase();
-                    return rr;
-                }, []);
-                return acc;
-            }
-
-            const val = acc.posToName.reduce((acc, name, i) => {
-                    acc[name] = r[i] || '';
-                    return acc;
-                }, {});
-            //if (val.date)
-                acc.res.push(val);
+        const fromRentSheet = rentData.reduce((acc, r, i) => {
+            if (!i) return acc;
+            acc.push({
+                date: moment(r[0], 'M/D/YYYY'),
+                amount: parseFloat(r[1].replace('$', '').replace(',', '').trim()).toFixed(2),
+                house: r[2],
+                comment: r[3],
+                row: i,
+            })
             return acc;
-        }, {
-            posToName: null,
-            res: [],
-            end: false,
+        }, []);
+
+
+        const payments = await db.doQuery(`select paymentID, receivedDate date, receivedAmount amount,
+         paidBy name,
+        notes, h.address
+        from rentPaymentInfo rp inner join leaseInfo l on rp.leaseID=l.leaseID
+        inner join houseInfo h on h.houseID=l.houseID`);
+
+        const paymentsByAmount = payments.reduce((acc, p) => {
+            const amount = p.amount.toFixed(2);
+            p.amount = amount;
+            p.date = moment(p.date);
+            let rows = (acc[amount]);
+            if (!rows) {
+                rows = [];
+                acc[amount] = rows;
+            }
+            rows.push(p);
+            return acc; 
         });
 
+        const mp = fromRentSheet.map(p => {
+            const matched = paymentsByAmount[p.amount];
+            console.log(`matched by amount ` + p.amount);
 
-
-        //console.log(result.res);
-
-        const cats = (await db.doQuery('select * from expenseCategories')).reduce((acc, k) => {
-            acc[k.expenseCategoryName] = k.expenseCategoryID;
-            return acc;
-        }, {});
-
-        const houses = (await db.doQuery('select * from houseInfo')).reduce((acc, h) => {
-            acc[h.address] = h;
-            return acc;
-        }, {});
-
-        const workers = (await db.doQuery('select * from workerInfo')).reduce((acc, h) => {
-            acc[`${h.firstName} ${h.lastName||''}`.trim()] = h.workerID;
-            return acc;
-        }, {});
-        console.log('start');
-        const sqlFreeForm = db.doQuery;
-        const xieOwnerId = (await db.doQueryOneRow(`select ownerID from ownerInfo where shortName='Xie'`))['ownerID'];
-        console.log(`xieOwernId=${xieOwnerId}`);
-        console.log(xieOwnerId)
-        return await Promise.map(result.res, async data => {
-            let categoryID = cats[data.category];
-            if (data.category && !categoryID) {
-                console.log('Creating category ' + data.category);
-                categoryID = uuid.v1();
-                cats[data.category] = categoryID;
-                await sqlFreeForm(`insert into expenseCategories(expenseCategoryID,expenseCategoryName) values
-                ('${categoryID}','${data.category}')`);
-            }
-
-            let houseID = '';
-            if (data.house) {
-                const curHouse = houses[data.house];
-                if (!curHouse) {
-                    console.log('Creating house ' + data.house);
-                    houseID = uuid.v1();
-                    houses[data.house] = { houseID };
-                    await sqlFreeForm(`insert into houseInfo(houseID,address) values
-                ('${houseID}','${data.house}')`);
-                } else if (!curHouse.ownerID) {
-                    await sqlFreeForm(`update houseInfo set ownerID = '${xieOwnerId}' where houseID='${curHouse.houseID}'`);
-                    curHouse.ownerID = xieOwnerId;
+            if (!matched) return;
+            const first = matched.find(m => {
+                if (m.matched) return null;
+                const diff = Math.abs(m.date.diff(p.date, 'days'));
+                if (diff < 3) {
+                    m.matched = true;
+                    m.diff = m.date.diff(p.date, 'days');
+                    m.row = p.row;
+                    return m;
                 }
-                if (curHouse) {
-                    houseID = curHouse.houseID;
-                }
-            }
+            });
+            return first;
+        }).filter(x => x);
 
-            let workerID = '';
-            if (data.worker) {
-                const fl = data.worker.split(' ').map(n => n.trim());
-                const tag = `${fl[0]} ${fl[1] || ''}`.trim();
-                workerID = workers[tag];
-                if (!workerID) {
-                    workerID = uuid.v1();
-                    console.log(`creating worker ${tag}`);
-                    await sqlFreeForm(`insert into workerInfo(workerID,firstName, lastName) values
-                ('${workerID}','${fl[0]}','${fl[1]||''}')`);
-                    workers[tag] = id;
-                } 
-            }
-            
+        console.log(mp.map(m => `${m.amount} ${m.date.format('YYYY-MM-DD')} ${m.diff} ${m.row}`));
 
-            const mdate = moment(data.date, 'M/D/YYYY');
-            if (!mdate.isValid()) return 0;
-            const date = mdate.format('YYYY-MM-DD')
-            const month = mdate.clone().startOf('month').format('YYYY-MM-DD');
-            const amount = data.amount.replace('$', '').replace(',', '').trim();
-            const mrs = await sqlFreeForm(`select maintenanceID from maintenanceRecords 
-            where date=? and description=? and houseID=? and workerID=? and expenseCategoryId=?
-             and amount=? and comment=?`, [date, data.description,
-                houseID, workerID, categoryID,
-            amount,data.comments]);
-            if (!mrs[0]) {
-                const id = uuid.v1();
-                console.log( [id, date, month, data.description,
-                    houseID, workerID, categoryID,
-                amount, data.comments])
-                await sqlFreeForm(`insert into maintenanceRecords(
-                    maintenanceID, date, month,description, houseID, workerID, expenseCategoryId, 
-                    amount, comment)
-        values(?,?,?,?, ?,?,?, ?,?)`, [id, date, month,data.description,
-                    houseID, workerID, categoryID,
-                    amount || 0, data.comments]);
-                return 1;
-            } 
-            
-            return 0;
-        }, { concurrency: 1 }).catch(err => {
-            console.log(err);
-        });
+
+        await Promise.map(mp, async m => {
+            await sheet.updateSheet(sheetId, [`'Copy of Sheet11'!E${m.row}:I${m.row}`], [
+               [m.date.format('YYYY-MM-DD'), m.amount, m.address, m.name, m.notes]
+            ]);
+        },{concurrency: 1});
+        
+        
     });
 }
 
