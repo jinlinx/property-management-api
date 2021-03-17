c81263f7f8195eaeae9f79fedcb638f0f11b496d
Gang Zhang <gzhangx@hotmail.com>
2021-03-17 00:08:56 -0400

    actively create field mapping

--- api/models/index.js
+++ api/models/index.js
@@ -1,10 +1,21 @@
 //const tenantInfo = require('./tenantInfo');
 const fs = require('fs');
+const keyBy = require('lodash/keyBy');
 const files = fs.readdirSync(__dirname).filter(n => n !== 'index.js');
 
+
+function createFieldMap(model) {
+    if(!model.fieldMap) {    
+      model.fieldMap = keyBy(model.fields, 'field');
+    }
+  }
+
+  
 const data = files.reduce((acc, fname) => {
     const modName = fname.split('.')[0];
-    acc[modName] = require(`./${modName}`);
+    const model = require(`./${modName}`);
+    createFieldMap(model);
+    acc[modName] = model;
     return acc;   
 }, {});
 
