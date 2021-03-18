82ff8dd7d7db7104e5b7f45e31feebd55e3877d1
Gang Zhang <gzhangx@hotmail.com>
2021-03-17 23:14:09 -0400

    fixing leaseTeanatInfo

--- api/models/leaseTenantInfo.js
+++ api/models/leaseTenantInfo.js
@@ -5,4 +5,17 @@ module.exports = {
             { field: 'tenantID', desc: 'Tenant Id', type: 'uuid', required: true, foreignKey: { table: 'tenantInfo', field: 'tenantID' } },            
             { field: 'leaseID', desc: 'Lease ID', required: true, foreignKey: { table: 'leaseInfo', field: 'leaseID' } },
         ],
+        view:{
+            name: 'view_leaseTenantInfo',
+            fields:[
+                { name: 'comment', field: 'comment', desc: 'Lease Comment', table: 'l' },
+                { name: 'address', field: 'address', desc: 'House', table: 'h' },
+
+                { field: 'firstName', desc: 'First Name', table:'tenantInfo' },
+                { field: 'lastName', desc: 'Last Name', table:'tenantInfo' },
+                { field: 'email', desc: 'Email', table:'tenantInfo' },
+                { field: 'phone', desc: 'Phone', table:'tenantInfo' },
+            ], 
+            extraViewJoins: ' left join tenantInfo t on t.tenantID=leaseTenantInfo.tenantID inner join leaseInfo l on l.leaseID=leaseTenantInfo.leaseID left join houseInfo h on h.houseID=l.houseID',
+        }
 };
