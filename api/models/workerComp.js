4ac8e2647b0ccdcc0c6606de56e909a81ccce42e
Gang Zhang <gzhangx@hotmail.com>
2021-05-31 13:16:27 -0400

    worker comp to use houseid

--- api/models/workerComp.js
+++ api/models/workerComp.js
@@ -6,16 +6,15 @@ module.exports={
             { field: 'dayOfMonth', desc: 'Day of Month to process' },
             {field: 'type', desc: 'Percent or amount'},
             {field: 'amount', desc: 'Percent', type: 'decimal'},
-            {field: 'leaseID', desc: 'Lease ID', foreignKey: {table: 'leaseInfo', field: 'leaseID'}},
+            {field: 'houseID', desc: 'House ID', foreignKey: {table: 'houseInfo', field: 'houseID'}},
         ],
         view:{
             name:'view_workerComp',
             fields:[
                 { name: 'firstName', field: 'firstName', desc: 'Worker Firstname', table: 'workerInfo' },
                 { name: 'lastName', field: 'lastName', desc: 'Worker Lastname', table: 'workerInfo' },
-                { name: 'comment', field: 'comment', desc: 'Lease Comment', table: 'leaseInfo' },
-                { field: 'address', desc: 'Address', table: 'h' },
+                { name: 'address', field: 'address', desc: 'Address', table: 'houseInfo' },
             ],  
-            extraViewJoins: ' left join houseInfo h on h.houseID = leaseInfo.houseID ',
+            //extraViewJoins: ' left join houseInfo h on h.houseID = leaseInfo.houseID ',
         }
 };
