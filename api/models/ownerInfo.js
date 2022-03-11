6dd918fe8531776567a9dab1e4eb8fff5754324e
Gang Zhang <gzhangx@hotmail.com>
2022-03-10 21:36:16 -0500

    add username

--- api/models/ownerInfo.js
+++ api/models/ownerInfo.js
@@ -2,7 +2,8 @@ module.exports = {
     fields:
         [
             { field: 'ownerID', desc: 'Id', type: 'ident', required: true, isId: true },
-            { field: 'ownerName', desc: 'Owner Name', required: true, unique: true, },
+            { field: 'ownerName', desc: 'Owner Name', required: true, },
+            { field: 'username', desc: 'username', required: true, unique: true, },
             { field: 'password', desc:'Password', required: false },
             { field: 'shortName', desc: 'Short Name', require: true },
         ]
