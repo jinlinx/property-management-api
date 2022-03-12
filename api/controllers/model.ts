c5cc85865ceb33a30b3a4e0049c3402bca594db7
Gang Zhang <gzhangx@hotmail.com>
2022-03-12 01:43:14 -0500

    ts

--- api/controllers/model.ts
+++ api/controllers/model.ts
@@ -1,12 +1,12 @@
-const models = require('../models/index');
-
-async function getModel(req, res) {
+import {data} from '../models/index';
+import {Request, Response} from 'restify'
+async function getModel(req:Request, res:Response) {
   try {
     console.log(`getModel -> ${req.query.name}`);
-        const m = models[req.query.name];
+        const m = data[req.query.name];
       console.log(`getModel done ${req.query.name}`);
       return res.json(m);
-    } catch (err) {
+    } catch (err: any) {
       console.log('getModel Error');
       console.log(err);
       res.send(500, {
