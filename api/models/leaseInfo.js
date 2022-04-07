2fc389dbde4ebc01bd3cad87b7d7274137b672a7
Gang Zhang <gzhangx@hotmail.com>
2022-04-07 19:05:58 -0400

    fixes

--- api/models/leaseInfo.js
+++ api/models/leaseInfo.js
@@ -3,13 +3,15 @@ module.exports = {
     fields:
         [
             { field: 'leaseID', desc: 'Id', type: 'uuid', required: true, isId: true },
-            { field: 'deposit', desc: 'Deposit',  },
+            { field: 'deposit', desc: 'Deposit', type: 'decimal' },
+            { field: 'petDeposit', desc: 'Pet Deposit', type: 'decimal', def: 0 },
+            { field: 'otherDeposit', desc: 'Other Deposit',  type: 'decimal', def: 0 },
             { field: 'endDate', desc: 'End Date',  type: 'date'},
             { field: 'startDate', desc: 'Start Date',  type: 'date'},
-            { field: 'houseID', desc: 'House ID', foreignKey: {table: 'houseInfo', field:'houseID'}},
+            { field: 'houseID', desc: 'House ID', foreignKey: { table: 'houseInfo', field: 'houseID' } },
+            { field: 'ownerID', type: 'int', desc: 'Owner', foreignKey: { table: 'ownerInfo', field: 'ownerID' }, required: true, def:'0', isOwnerSecurityField: true,},
             { field: 'comment', desc: 'Comment' },
-            { field: 'monthlyRent', desc: 'Monthly Rent', required: true, type: 'decimal', },
-            { field: 'vdPosControl', desc: 'PosControl' },
+            { field: 'monthlyRent', desc: 'Monthly Rent', required: true, type: 'decimal', },            
         ],
         view:{
             name:'view_leaseInfo',
--- api/models/tenantInfo.js
+++ api/models/tenantInfo.js
@@ -4,6 +4,7 @@ module.exports = {
             { field: 'tenantID', desc: 'Id', type: 'uuid', required: true, isId: true },
             { field: 'firstName', desc: 'First Name', required: true },
             { field: 'lastName', desc: 'Last Name', required: true },
+            { field: 'fullName', desc: 'Full Name', required: true, def: '' },
             { field: 'email', desc: 'Email', },
             { field: 'phone', desc: 'Phone', },
             { field: 'ssn', desc: 'SSN', },
@@ -14,6 +15,5 @@ module.exports = {
             { field: 'dadName', desc: 'Dad Name', },
             { field: 'dadPhone', desc: 'Dad Phone', },
             { field: 'ownerID', type: 'int', desc: 'Owner', foreignKey: { table: 'ownerInfo', field: 'ownerID' }, required: true, def:'0', isOwnerSecurityField: true,},
-            { field: 'vdPosControl', desc: 'PosControl' },
         ]
 };
