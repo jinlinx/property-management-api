e00f10e9e37981ac4bc75ca00488f2c6a37b5d6d
Gang Zhang <gzhangx@hotmail.com>
2024-11-19 17:32:11 -0500

    fix test

--- tests/test.ts
+++ tests/test.ts
@@ -1,12 +1,16 @@
-import gs from '@gzhangx/googleapi'
+import * as gs from '@gzhangx/googleapi'
 import * as fs from 'fs'
+import * as db from '../api/lib/db'
 
 function getCreds() {
     const creds: gs.gsAccount.IServiceAccountCreds
-    = JSON.parse(fs.readFileSync('c:\\work\\creds\\gzperm-pmapi-googleSvcAccount-aa12f63bd789.json').toString());
+    = JSON.parse(fs.readFileSync('d:\\work\\cur\\creds\\gzperm-pmapi-googleSvcAccount-aa12f63bd789.json').toString());
     return creds;
 }
 async function test() {
+
+    const dbres = await db.findUser({ id: '1' });
+    console.log('dbres',dbres);
     const client = gs.gsAccount.getClient(getCreds());
     const ops = client.getSheetOps("13Mu-zB_WicsI3JXcFCLzvYTE-brVqVXnbDzgoIseF34");
     const rows = await ops.readDataByColumnName("MaintainessRecord");
