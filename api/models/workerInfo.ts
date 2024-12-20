1a1f772be2ebf306e17f13c831ac67d529c9157e
Gang Zhang <gzhangx@hotmail.com>
2024-12-20 10:06:15 -0500

    new worker model

--- api/models/workerInfo.ts
+++ api/models/workerInfo.ts
@@ -2,13 +2,16 @@ import { IDBModel } from './types';
 export const workerInfo: IDBModel = {
     fields:
         [
-            {field: 'workerID',desc: 'Id',type: 'uuid',required: true,isId: true},
-            {field: 'firstName',desc: 'First Name',required: true},
-            {field: 'lastName',desc: 'Last Name',required: true},
-            {field: 'email',desc: 'Email',},
-            {field: 'phone',desc: 'Phone',},
-            {field: 'taxID',desc: 'SSN',},
-            { field: 'address', desc: 'Address', },
-            { field: 'vdPosControl', desc: 'PosControl' },
+            { field: 'userID', desc: 'Owner', foreignKey: { table: 'userInfo', field: 'userID' }, required: true,},
+            { field: 'workerID',desc: 'Id',type: 'uuid',required: true, isId: true},
+            { field: 'workerName',desc: 'Worker Name', required: true },
+            { field: 'taxName', desc: 'Tax Name' },
+            { field: 'taxID', desc: 'SSN', },
+            { field: 'address', desc: 'Address' },
+            { field: 'city', desc: 'City' },
+            { field: 'state', desc: 'State' },
+            { field: 'zip', desc: 'Zip' },
+            { field: 'email',desc: 'Email',},
+            { field: 'phone',desc: 'Phone',},
         ]
 };
