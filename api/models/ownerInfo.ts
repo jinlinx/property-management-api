fdf2dc5f5872886c33f45f33e767136abca10638
Gang Zhang <gzhangx@hotmail.com>
2024-12-20 20:28:17 -0500

    added owner info

---
+++ api/models/ownerInfo.ts
@@ -0,0 +1,16 @@
+import { IDBModel } from './types';
+export const ownerInfo: IDBModel = {
+    fields:
+        [
+            { field: 'userID', desc: 'Owner', foreignKey: { table: 'userInfo', field: 'userID' }, required: true, isId: true,},
+            { field: 'ownerName',desc: 'Owner Name', required: true },
+            { field: 'taxName', desc: 'Tax Name' },
+            { field: 'taxID', desc: 'SSN', },
+            { field: 'address', desc: 'Address' },
+            { field: 'city', desc: 'City' },
+            { field: 'state', desc: 'State' },
+            { field: 'zip', desc: 'Zip' },
+            { field: 'email',desc: 'Email',},
+            { field: 'phone',desc: 'Phone',},
+        ]
+};
